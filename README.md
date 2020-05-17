b627de67b8acf0691863cc7fa403d76c61552774
Aditya <taplejung@protonmail.ch>
2020-05-17 10:35:31 +0100

    Add ban command and readme

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
-# allalin-bot
-A slightly unorthodox discord moderation bot
+# Allalin
+[![License](https://img.shields.io/github/license/deekts/allalin-bot)](LICENSE)
+[![StandardJS](https://img.shields.io/badge/code_style-standard-brightgreen)](https://standardjs.com/)
+
+Allalin is an extensible discord bot.
+
+## Installation
+* Install [Node.js](https://nodejs.org/) and [npm](https://www.npmjs.com/)
+* Install [Discord.js](https://discord.js.org/)
+* Clone the repository
+* [Create a discord bot](https://discord.com/developers)
+* Copy `config.json.template` to `config.json`
+* Replace the token placeholder with your bot's token
--- commands/ban.js
+++ commands/ban.js
@@ -1,16 +1,16 @@
 module.exports = {
-  name: 'kick',
-  description: 'Kick a member from the guild.',
-  usage: 'kick [mention]',
+  name: 'ban',
+  description: 'Ban a member from the guild.',
+  usage: 'ban [mention]',
   argsreq: true,
   mentionreq: true,
-  permissions: ['KICK_MEMBERS'],
+  permissions: ['BAN_MEMBERS'],
   async execute (message, args) {
     const target = await message.guild.members.fetch(message.mentions.users.first().id)
 
-    if (!target.kickable) return message.reply(`${target.user.tag} cannot be kicked.`)
+    if (!target.bannable) return message.reply(`${target.user.tag} cannot be banned.`)
 
-    message.reply(`do you want to kick ${target.user.tag}? \`yes\`/\`no\``)
+    message.reply(`do you want to ban ${target.user.tag}? \`yes\`/\`no\``)
 
     const filter = m => {
       return m.author.id === message.author.id
@@ -21,10 +21,10 @@ module.exports = {
     collector.on('collect', m => {
       if (m.content.includes('yes')) {
         target.kick()
-        message.reply(`kicked ${target.user.tag} (${target.user.id}) successfully.`)
+        message.reply(`banned ${target.user.tag} (${target.user.id}) successfully.`)
         collector.stop()
       } else if (m.content.includes('no')) {
-        message.reply(`cancelled kicking of ${target.user.tag}.`)
+        message.reply(`cancelled banning of ${target.user.tag}.`)
         collector.stop()
       } else {
         message.reply('please send either `yes` or `no`.')
